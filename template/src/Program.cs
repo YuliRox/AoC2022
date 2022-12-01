ba2ee8d478c65b997f343f1d4482e0bf5d20488d
Xemrox <xemrox@user.noreply.github.com>
2022-12-01 21:01:59 +0100

    (chore) converted to file scoped namespace

--- template/src/Program.cs
+++ template/src/Program.cs
@@ -3,46 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using Shared;
 
-namespace src
+namespace src;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+
+        var inputData = DataLoader.LoadInputData();
+
+        foreach (var inputSet in inputData)
         {
-
-            var inputData = DataLoader.LoadInputData();
-
-            foreach (var inputSet in inputData)
+            if (!inputSet.Content.Any())
             {
-                if (!inputSet.Content.Any())
-                {
-                    continue;
-                }
-                Console.WriteLine($"> Part-1 for {inputSet.Name}");
-
-                var value = Part1(inputSet);
-                Console.WriteLine("Value: {0}", value);
-
-                Console.WriteLine($"< Part-1 for {inputSet.Name}");
-
-
-                Console.WriteLine($"> Part-2 for {inputSet.Name}");
-
-                var value2 = Part2(inputSet);
-                Console.WriteLine("Value: {0}", value2);
-
-                Console.WriteLine($"< Part-2 for {inputSet.Name}");
+                continue;
             }
-        }
+            Console.WriteLine($"> Part-1 for {inputSet.Name}");
 
-        private static int Part1(PuzzleInput<string> input)
-        {
-            return 0;
-        }
+            var value = Part1(inputSet);
+            Console.WriteLine("Value: {0}", value);
 
-        private static int Part2(PuzzleInput<string> input)
-        {
-            return 0;
+            Console.WriteLine($"< Part-1 for {inputSet.Name}");
+
+
+            Console.WriteLine($"> Part-2 for {inputSet.Name}");
+
+            var value2 = Part2(inputSet);
+            Console.WriteLine("Value: {0}", value2);
+
+            Console.WriteLine($"< Part-2 for {inputSet.Name}");
         }
+    }
+
+    private static int Part1(PuzzleInput<string> input)
+    {
+        return 0;
+    }
+
+    private static int Part2(PuzzleInput<string> input)
+    {
+        return 0;
     }
 }
