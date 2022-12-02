2b0c196d76383c39086691d1c76dd079472560df
Sandra Schuhmacher <sandra.schuhmacher@sva.de>
2022-12-02 09:04:37 +0100

    (chore) day 2 - add initial template

---
+++ 02/RockPaperScissors/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Shared;
+
+namespace RockPaperScissors;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var inputData = DataLoader.LoadInputData();
+
+        foreach (var inputSet in inputData)
+        {
+            if (!inputSet.Content.Any())
+            {
+                continue;
+            }
+            Console.WriteLine($"> Part-1 for {inputSet.Name}");
+
+            var value = Part1(inputSet);
+            Console.WriteLine("Value: {0}", value);
+
+            Console.WriteLine($"< Part-1 for {inputSet.Name}");
+
+
+            Console.WriteLine($"> Part-2 for {inputSet.Name}");
+
+            var value2 = Part2(inputSet);
+            Console.WriteLine("Value: {0}", value2);
+
+            Console.WriteLine($"< Part-2 for {inputSet.Name}");
+        }
+    }
+
+    private static int Part1(PuzzleInput<string> input)
+    {
+        return 0;
+    }
+
+    private static int Part2(PuzzleInput<string> input)
+    {
+        return 0;
+    }
+}
