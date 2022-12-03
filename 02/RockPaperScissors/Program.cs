f37a74c6c8c1ca9aaa6f11950f2d6abf635a0b4a
Sandra Schuhmacher <sandra.schuhmacher@sva.de>
2022-12-03 17:47:05 +0100

    (feat) add initial datastructure and load input into it

--- 02/RockPaperScissors/Program.cs
+++ 02/RockPaperScissors/Program.cs
@@ -10,7 +10,11 @@ class Program
     static void Main(string[] args)
     {
 
-        var inputData = DataLoader.LoadInputData();
+        var inputData = DataLoader.LoadInputData<Round>(r =>
+        {
+            var input = r.Split(" ").Select(SymbolFromString);
+            return new Round(input.First(), input.Last());
+        });
 
         foreach (var inputSet in inputData)
         {
@@ -35,13 +39,41 @@ class Program
         }
     }
 
-    private static int Part1(PuzzleInput<string> input)
+    private static int Part1(PuzzleInput<Round> input)
     {
         return 0;
     }
 
-    private static int Part2(PuzzleInput<string> input)
+    private static int Part2(PuzzleInput<Round> input)
     {
         return 0;
     }
+
+    public readonly record struct Round
+    {
+        public Round(Symbol ownChoice, Symbol opponentChoice)
+        {
+            OwnChoice = ownChoice;
+            OpponentChoice = opponentChoice;
+        }
+
+        public readonly Symbol OwnChoice { get; init; }
+        public readonly Symbol OpponentChoice { get; init; }
+
+    }
+    public static Symbol SymbolFromString(string rawInput) => rawInput switch
+    {
+        "A" or "X" => Symbol.Rock,
+        "B" or "Y" => Symbol.Paper,
+        "C" or "Z" => Symbol.Scissors,
+        _ => throw new ArgumentException(nameof(rawInput))
+    };
+
+    public enum Symbol
+    {
+        Rock = 1,
+        Paper = 2,
+        Scissors = 3
+    }
+
 }
