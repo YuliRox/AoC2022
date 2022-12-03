8e40ebbe6e635baaf5694f8250d61144e4cb6c62
Sandra Schuhmacher <sandra.schuhmacher@sva.de>
2022-12-03 17:59:33 +0100

    (feat) add game solver to score rounds

--- 02/RockPaperScissors/Program.cs
+++ 02/RockPaperScissors/Program.cs
@@ -25,7 +25,7 @@ class Program
             Console.WriteLine($"> Part-1 for {inputSet.Name}");
 
             var value = Part1(inputSet);
-            Console.WriteLine("Value: {0}", value);
+            Console.WriteLine("Total Score: {0}", value);
 
             Console.WriteLine($"< Part-1 for {inputSet.Name}");
 
@@ -41,7 +41,7 @@ class Program
 
     private static int Part1(PuzzleInput<Round> input)
     {
-        return 0;
+        return input.Content.Select(r => GameSolver.ResolveRound(r)).Sum();
     }
 
     private static int Part2(PuzzleInput<Round> input)
@@ -74,6 +74,33 @@ class Program
         Rock = 1,
         Paper = 2,
         Scissors = 3
+    }
+
+    public enum GameResult{
+        Lost = 0,
+        Draw = 3,
+        Won = 6
+    }
+
+    public static class GameSolver {
+
+        public static int ResolveRound(Round round){
+            if (round.OwnChoice == round.OpponentChoice) {
+                return (int) GameResult.Draw + (int) round.OwnChoice;
+            }
+            else if (
+                (round.OwnChoice == Symbol.Rock && round.OpponentChoice == Symbol.Scissors) ||
+                (round.OwnChoice == Symbol.Scissors && round.OpponentChoice == Symbol.Paper) ||
+                (round.OwnChoice == Symbol.Paper && round.OpponentChoice == Symbol.Rock)
+            ){
+                return (int)GameResult.Won + (int)round.OwnChoice;
+            } else {
+                return (int)GameResult.Lost + (int)round.OwnChoice;
+            }
+
+        }
+    
+
     }
 
 }
