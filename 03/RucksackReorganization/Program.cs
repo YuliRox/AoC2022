88d1839f48a81006ab28f9f24fef5dd43411872d
Xemrox <xemrox@user.noreply.github.com>
2022-12-03 20:33:13 +0100

    (feat) added solution for day-03 part 1

--- 03/RucksackReorganization/Program.cs
+++ 03/RucksackReorganization/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Text.Unicode;
 using Shared;
 
 namespace RucksackReorganization;
@@ -9,8 +11,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine('a' + 'a');
 
-        var inputData = DataLoader.LoadInputData();
+        var inputData = DataLoader.LoadInputData(line => new Rucksack(line));
 
         foreach (var inputSet in inputData)
         {
@@ -35,13 +38,78 @@ class Program
         }
     }
 
-    private static int Part1(PuzzleInput<string> input)
+    private static int Part1(PuzzleInput<Rucksack> input)
     {
+        var translator = new PriorityTranslator();
+        return input.Content
+            .Select(x => x.FindDuplicateItem())
+            .Select(translator.GetPriority)
+            .Sum();
+
+        foreach (var rucksack in input.Content)
+        {
+            var item = rucksack.FindDuplicateItem();
+            var priority = (int)item;
+        }
+
         return 0;
     }
 
-    private static int Part2(PuzzleInput<string> input)
+    private static int Part2(PuzzleInput<Rucksack> input)
     {
         return 0;
+    }
+}
+
+readonly record struct Rucksack
+{
+    public string Items { get; init; }
+    public ReadOnlyMemory<char> ItemList { get; init; }
+    public ReadOnlyMemory<char> FirstCompartment { get; init; }
+    public ReadOnlyMemory<char> SecondCompartment { get; init; }
+
+    public Rucksack(string itemList)
+    {
+        Items = itemList;
+        ItemList = Items.AsMemory();
+        var midPoint = ItemList.Length / 2;
+        FirstCompartment = ItemList[0..midPoint];
+        SecondCompartment = ItemList[midPoint..];
+    }
+
+    public char FindDuplicateItem()
+    {
+        var firstSegment = FirstCompartment.Span;
+        var secondSegment = SecondCompartment.Span;
+
+        foreach (var firstItem in firstSegment)
+        {
+            foreach (var secondItem in secondSegment)
+            {
+                if (firstItem == secondItem)
+                {
+                    return firstItem;
+                }
+            }
+        }
+        return char.MinValue;
+    }
+}
+
+class PriorityTranslator
+{
+    private readonly Dictionary<char, int> _priorities;
+    private const string charRange = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public PriorityTranslator()
+    {
+        _priorities = Enumerable
+            .Range(0, charRange.Length)
+            .ToDictionary(index => charRange[index], index => index + 1);
+    }
+
+    public int GetPriority(char item)
+    {
+        return _priorities[item];
     }
 }
