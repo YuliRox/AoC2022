5956d123ad73b189d530d3f6b93e1554432f1690
Sandra Schuhmacher <sandra.schuhmacher@sva.de>
2022-12-03 19:12:03 +0100

    (feature) refactor dto to work for both parts, solve part 2

--- 02/RockPaperScissors/Program.cs
+++ 02/RockPaperScissors/Program.cs
@@ -12,7 +12,7 @@ class Program
 
         var inputData = DataLoader.LoadInputData<Round>(r =>
         {
-            var input = r.Split(" ").Select(SymbolFromString);
+            var input = r.Split(" ");
             return new Round(input.First(), input.Last());
         });
 
@@ -41,26 +41,97 @@ class Program
 
     private static int Part1(PuzzleInput<Round> input)
     {
-        return input.Content.Select(r => GameSolver.ResolveRound(r)).Sum();
+        return input.Content.Select(r => GameSolver.ResolveRoundByChoices(r)).Sum();
     }
 
     private static int Part2(PuzzleInput<Round> input)
     {
-        return 0;
+        return input.Content.Select(r => GameSolver.ResolveRoundByOutcome(r)).Sum();
     }
 
-    public readonly record struct Round
+}
+
+public readonly record struct Round
+{
+    public Round(string opponentChoice, string strategyChoice)
     {
-        public Round(Symbol opponentChoice, Symbol ownChoice)
-        {
-            OwnChoice = ownChoice;
-            OpponentChoice = opponentChoice;
-        }
-
-        public readonly Symbol OwnChoice { get; init; }
-        public readonly Symbol OpponentChoice { get; init; }
-
+        StrategyChoice = strategyChoice;
+        OpponentChoice = opponentChoice;
     }
+
+    public readonly string StrategyChoice { get; init; }
+    public readonly string OpponentChoice { get; init; }
+
+}
+
+public enum Symbol
+{
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3
+}
+
+public enum GameResult
+{
+    Lost = 0,
+    Draw = 3,
+    Won = 6
+}
+
+public static class GameSolver
+{
+
+    public static int ResolveRoundByChoices(Round round)
+    {
+        var ownChoice = SymbolFromString(round.StrategyChoice);
+        var opponentChoice = SymbolFromString(round.OpponentChoice);
+
+        if (ownChoice == opponentChoice)
+        {
+            return (int)GameResult.Draw + (int)ownChoice;
+        }
+        else if (
+            (ownChoice == Symbol.Rock && opponentChoice == Symbol.Scissors) ||
+            (ownChoice == Symbol.Scissors && opponentChoice == Symbol.Paper) ||
+            (ownChoice == Symbol.Paper && opponentChoice == Symbol.Rock)
+        )
+        {
+            return (int)GameResult.Won + (int)ownChoice;
+        }
+        else
+        {
+            return (int)GameResult.Lost + (int)ownChoice;
+        }
+    }
+
+    public static int ResolveRoundByOutcome(Round round)
+    {
+        var opponentChoice = SymbolFromString(round.OpponentChoice);
+        var gameResult = ResultFromString(round.StrategyChoice);
+        var gameInstruction = (opponentChoice, gameResult);
+
+        return gameInstruction switch
+        {
+            (_, GameResult.Draw) => (int)GameResult.Draw + (int)gameInstruction.opponentChoice,
+            (Symbol.Rock, GameResult.Won) => (int)GameResult.Won + (int)Symbol.Paper,
+            (Symbol.Paper, GameResult.Won) => (int)GameResult.Won + (int)Symbol.Scissors,
+            (Symbol.Scissors, GameResult.Won) => (int)GameResult.Won + (int)Symbol.Rock,
+            (Symbol.Rock, GameResult.Lost) => (int)GameResult.Lost + (int)Symbol.Scissors,
+            (Symbol.Paper, GameResult.Lost) => (int)GameResult.Lost + (int)Symbol.Rock,
+            (Symbol.Scissors, GameResult.Lost) => (int)GameResult.Lost + (int)Symbol.Paper,
+            _ => throw new ArgumentException("Invalid combination of symbols", nameof(round))
+        };
+    }
+
+    private static GameResult ResultFromString(string strategyChoice) => strategyChoice switch
+    {
+        "X" => GameResult.Lost,
+        "Y" => GameResult.Draw,
+        "Z" => GameResult.Won,
+        _ => throw new ArgumentException("Invalid input symbol", nameof(strategyChoice))
+    };
+
+
     public static Symbol SymbolFromString(string rawInput) => rawInput switch
     {
         "A" or "X" => Symbol.Rock,
@@ -69,41 +140,4 @@ class Program
         _ => throw new ArgumentException("Invalid input symbol", nameof(rawInput))
     };
 
-    public enum Symbol
-    {
-        Rock = 1,
-        Paper = 2,
-        Scissors = 3
-    }
-
-    public enum GameResult
-    {
-        Lost = 0,
-        Draw = 3,
-        Won = 6
-    }
-
-    public static class GameSolver
-    {
-
-        public static int ResolveRound(Round round)
-        {
-            if (round.OwnChoice == round.OpponentChoice)
-            {
-                return (int)GameResult.Draw + (int)round.OwnChoice;
-            }
-            else if (
-                (round.OwnChoice == Symbol.Rock && round.OpponentChoice == Symbol.Scissors) ||
-                (round.OwnChoice == Symbol.Scissors && round.OpponentChoice == Symbol.Paper) ||
-                (round.OwnChoice == Symbol.Paper && round.OpponentChoice == Symbol.Rock)
-            )
-            {
-                return (int)GameResult.Won + (int)round.OwnChoice;
-            }
-            else
-            {
-                return (int)GameResult.Lost + (int)round.OwnChoice;
-            }
-        }
-    }
 }
